1346516719e4cdd8fdfb74e9cb07fce1af7fe42c
Eduardo Mandry <eduardo.mandry@amaris.com>
2021-02-08 17:11:07 -0300

    First commit with action

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# aws-ecr-policy-action
-Action for creating and modify the policy
+# AWS ECR With Policy Creation Action
+
+# WIP... NOT A FINAL VERSION ...
+
+This Action allows you to create Docker images and push into a ECR repository.
+
+## Parameters
+| Parameter | Type | Default | Description |
+|-----------|------|---------|-------------|
+| `access_key_id` | `string` | | Your AWS access key id |
+| `secret_access_key` | `string` | | Your AWS secret access key |
+| `account_id` | `string` | | Your AWS Account ID |
+| `repo` | `string` | | Name of your ECR repository |
+| `region` | `string` | | Your AWS region |
+| `create_repo` | `boolean` | `false` | Set this to true to create the repository if it does not already exist |
+| `tags` | `string` | `latest` | Comma-separated string of ECR image tags (ex latest,1.0.0,) |
+| `dockerfile` | `string` | `Dockerfile` | Name of Dockerfile to use |
+| `extra_build_args` | `string` | `""` | Extra flags to pass to docker build (see docs.docker.com/engine/reference/commandline/build) |
+| `path` | `string` | `.` | Path to Dockerfile, defaults to the working directory |
+| `prebuild_script` | `string` | | Relative path from top-level to script to run before Docker build |
+
+## Usage
+```yaml
+jobs:
+  build-and-push:
+    runs-on: ubuntu-latests
+    steps:
+    - uses: kciter/aws-ecr-action@master
+      with:
+        access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+        secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+        account_id: ${{ secrets.AWS_ACCOUNT_ID }}
+        repo: docker/repo
+        region: ap-northeast-2
+        tags: latest,${{ github.sha }}
+        create_repo: true
+```
+
+## Reference
+* https://github.com/CircleCI-Public/aws-ecr-orb
+* https://github.com/elgohr/Publish-Docker-Github-Action
+
+## License
+The MIT License (MIT)
---
+++ entrypoint.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -e
+
+function main() {
+  sanitize "${INPUT_ACCESS_KEY_ID}" "access_key_id"
+  sanitize "${INPUT_SECRET_ACCESS_KEY}" "secret_access_key"
+  sanitize "${INPUT_REGION}" "region"
+  sanitize "${INPUT_ACCOUNT_ID}" "account_id"
+  sanitize "${INPUT_ECR_REPOSITORY}" "INPUT_ECR_REPOSITORY"
+
+  ACCOUNT_URL="$INPUT_ACCOUNT_ID.dkr.ecr.$INPUT_REGION.amazonaws.com"
+
+  aws_configure
+  assume_role
+  //login
+  run_pre_build_script $INPUT_PREBUILD_SCRIPT
+  docker_build $INPUT_TAGS $INPUT_ECR_REGISTRY
+  create_ecr_repo $INPUT_CREATE_REPO
+  update_ecr_repo_policy $INPUT_POLICY
+  docker_push_to_ecr $INPUT_TAGS $INPUT_ECR_REGISTRY
+}
+
+function sanitize() {
+  if [ -z "${1}" ]; then
+    >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
+    exit 1
+  fi
+}
+
+function aws_configure() {
+  export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY
+  export AWS_DEFAULT_REGION=$INPUT_REGION
+}
+
+function login() {
+  echo "== START LOGIN"
+  LOGIN_COMMAND=$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+  $LOGIN_COMMAND
+  echo "== FINISHED LOGIN"
+}
+
+function assume_role() {
+  if [ "${INPUT_ASSUME_ROLE}" != "" ]; then
+    sanitize "${INPUT_ASSUME_ROLE}" "assume_role"
+    echo "== START ASSUME ROLE"
+    ROLE="arn:aws:iam::${INPUT_ACCOUNT_ID}:role/${INPUT_ASSUME_ROLE}"
+    CREDENTIALS=$(aws sts assume-role --role-arn ${ROLE} --role-session-name ecrpush --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text)
+    read id key token <<< ${CREDENTIALS}
+    export AWS_ACCESS_KEY_ID="${id}"
+    export AWS_SECRET_ACCESS_KEY="${key}"
+    export AWS_SESSION_TOKEN="${token}"
+    echo "== FINISHED ASSUME ROLE"
+  fi
+}
+
+function create_ecr_repo() {
+  if [ "${1}" = true ]; then
+    echo "== START CREATE REPO"
+    aws ecr describe-repositories --region $AWS_DEFAULT_REGION --repository-names $INPUT_ECR_REPOSITORY > /dev/null 2>&1 || \
+      aws ecr create-repository --region $AWS_DEFAULT_REGION --repository-name $INPUT_ECR_REPOSITORY '{ "rules": [ { "rulePriority": 1, "description": "Rule for keep Images", "selection": { "tagStatus": "any", "countType": "imageCountMoreThan", "countNumber": $(INPUT_POLICY) }, "action": { "type": "expire" } } ] }'
+    echo "== FINISHED CREATE REPO"
+  fi
+}
+
+function update_ecr_repo_policy() {
+  if [ "${1}" = true ]; then
+    echo "== START CREATE REPO"
+    aws ecr get-lifecycle-policy --repository-name $INPUT_ECR_REPOSITORY > /dev/null 2>&1 || \
+      aws ecr put-lifecycle-policy --repository-name $INPUT_ECR_REPOSITORY --lifecycle-policy-text '{ "rules": [ { "rulePriority": 1, "description": "Rule for keep Images", "selection": { "tagStatus": "any", "countType": "imageCountMoreThan", "countNumber": $(INPUT_POLICY) }, "action": { "type": "expire" } } ] }'
+    echo "== FINISHED CREATE REPO"
+  fi
+}
+
+function run_pre_build_script() {
+  if [ ! -z "${1}" ]; then
+    echo "== START PREBUILD SCRIPT"
+    chmod a+x $1
+    $1
+    echo "== FINISHED PREBUILD SCRIPT"
+  fi
+}
+
+function docker_build() {
+  echo "== START DOCKERIZE"
+  local TAG=$1
+  local docker_tag_args=""
+  local DOCKER_TAGS=$(echo "$TAG" | tr "," "\n")
+  for tag in $DOCKER_TAGS; do
+    docker_tag_args="$docker_tag_args -t $2/$INPUT_ECR_REPOSITORY:$tag"
+  done
+
+  docker build $INPUT_EXTRA_BUILD_ARGS -f $INPUT_DOCKERFILE $docker_tag_args $INPUT_PATH
+  echo "== FINISHED DOCKERIZE"
+}
+
+function docker_push_to_ecr() {
+  echo "== START PUSH TO ECR"
+  local TAG=$1
+  local DOCKER_TAGS=$(echo "$TAG" | tr "," "\n")
+  for tag in $DOCKER_TAGS; do
+    docker push $2/$INPUT_ECR_REPOSITORY:$tag
+    echo ::set-output name=image::$2/$INPUT_ECR_REPOSITORY:$tag
+  done
+  echo "== FINISHED PUSH TO ECR"
+}
+
+main
