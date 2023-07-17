1a2e420373d67950a59e6831880e623a2f0fa58c
fjleivad <fjleivad@smu.cl>
2023-07-17 18:18:31 -0400

    test aws fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,8 +57,8 @@ function sanitize() {
 }
 
 function aws_configure() {
-  export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID
-  export AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY
+  export AWS_ACCESS_KEY=$INPUT_ACCESS_KEY_ID
+  export AWS_SECRET_KEY=$INPUT_SECRET_ACCESS_KEY
   export AWS_DEFAULT_REGION=$INPUT_REGION
 }
 
