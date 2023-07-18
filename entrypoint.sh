c99b616820fbcc59b2e64bfe8430a229cc868c9b
Eduardo Mandry <78376595+emandry@users.noreply.github.com>
2023-07-17 23:18:30 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,9 +64,9 @@ function aws_configure() {
   mkdir /root/.aws
 
   echo "[default]" > /root/.aws/credentials
-  echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
-  echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
-  echo "aws_default_region=$AWS_DEFAULT_REGION" >> /root/.aws/credentials
+  echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
+  echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
+  echo "aws_default_region = $AWS_DEFAULT_REGION" >> /root/.aws/credentials
 
   docker pull amazon/aws-cli:2.0.6
  
