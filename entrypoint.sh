edaadb61a9fb9d7eae7e78171a43d7e345568a3c
Eduardo Mandry <78376595+emandry@users.noreply.github.com>
2023-07-18 08:36:38 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -123,9 +123,9 @@ function update_ecr_repo_policy() {
     echo "== END BUILD RULES"
     echo "== START CREATE REPO POLICY"
     docker run --rm -i -e AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY -e AWS_DEFAULT_OUTPUT=json amazon/aws-cli ecr get-lifecycle-policy --repository-name $INPUT_REPO > /dev/null 2>&1 && \
-    #docker run --rm -i -e AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY -e AWS_DEFAULT_OUTPUT=json amazon/aws-cli ecr delete-lifecycle-policy --repository-name $INPUT_REPO
-    #docker run --rm -i -e AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY -e AWS_DEFAULT_OUTPUT=json amazon/aws-cli ecr put-lifecycle-policy --repository-name $INPUT_REPO --lifecycle-policy-text "$ruleStart$ruleText$ruleEnd" 
-    #echo "== FINISHED CREATE REPO POLICY"
+    docker run --rm -i -e AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY -e AWS_DEFAULT_OUTPUT=json amazon/aws-cli ecr delete-lifecycle-policy --repository-name $INPUT_REPO
+    docker run --rm -i -e AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY -e AWS_DEFAULT_OUTPUT=json amazon/aws-cli ecr put-lifecycle-policy --repository-name $INPUT_REPO --lifecycle-policy-text "$ruleStart$ruleText$ruleEnd" 
+    echo "== FINISHED CREATE REPO POLICY"
   fi
 }
 
