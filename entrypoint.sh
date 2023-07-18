caccbaef301dfcfc10e23dd567241c5a63a82cfd
Eduardo Mandry <78376595+emandry@users.noreply.github.com>
2023-07-17 22:43:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,7 @@ function login() {
   echo "== START LOGIN"
   #LOGIN_COMMAND=$(aws ecr get-login-password --no-include-email --region $AWS_DEFAULT_REGION)
 
-  LOGIN_COMMAND=$(docker run --rm -ti -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli:2.0. ecr get-login-password --region $INPUT_REGION)
+  LOGIN_COMMAND=$(docker run --rm -i -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli:2.0.6 ecr get-login-password --region $INPUT_REGION)
   LOGIN_COMMAND1=$(docker login --username AWS --password $LOGIN_COMMAND $INPUT_ECR_REGISTRY)
   $LOGIN_COMMAND1
   
